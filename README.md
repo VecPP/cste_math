1b4a38cf8519b6ce3f78baf0da3eeb0c4436370d
Francois Chabot <francois.chabot.dev@gmail.com>
2018-09-06 11:39:25 -0400

    infrastructure + ceil/floor (#1)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cste_math
+
 constexpr scalar math
+
+## Origin
+
+cste_math was spun off from [VecPP](https://github.com/VecPP/vecpp). The
+distinction between compile-time and runtime algorithms was getting messy, so
+isolating the constexpr algorithms was needed. We decided we might as well make 
+it a distinct repository.
---
+++ include/cste_math/constants.h
@@ -0,0 +1,31 @@
+//  Copyright 2018 Francois Chabot
+//  (francois.chabot.dev@gmail.com)
+//
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CSTE_CONSTANTS_H_INCLUDED
+#define CSTE_CONSTANTS_H_INCLUDED
+
+#include "cste_math/config.h"
+
+namespace VECPP_NAMESPACE {
+
+// Constants are defined with enough precision for a Quadruple precision float
+// which means at least 36 decimal digits.
+
+template <typename T>
+constexpr T pi = T(3.14159265358979323846264338327950288419L);
+
+template <typename T>
+constexpr T half_pi = pi<T> / T(2);
+
+template <typename T>
+constexpr T quarter_pi = pi<T> / T(4);
+
+template <typename T>
+constexpr T two_pi = pi<T> * T(2);
+
+}  // namespace VECPP_NAMESPACE
+#endif
